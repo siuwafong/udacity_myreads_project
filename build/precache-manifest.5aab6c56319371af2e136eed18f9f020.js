a20ee3a4a12a668d45db747ce8a5f48ba8bc88a9
siuwafong <wilsonfong82@gmail.com>
2020-07-13 19:42:19 -0400

    change Book component from class to function

---
+++ build/precache-manifest.5aab6c56319371af2e136eed18f9f020.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ace2b6e2f121d297a448",
+    "url": "/static/js/main.ace2b6e2.chunk.js"
+  },
+  {
+    "revision": "6163b365ee52d863bef9",
+    "url": "/static/js/1.6163b365.chunk.js"
+  },
+  {
+    "revision": "ace2b6e2f121d297a448",
+    "url": "/static/css/main.16fb2a06.chunk.css"
+  },
+  {
+    "revision": "8aa818da4cd3bb209c276de60a8f2382",
+    "url": "/index.html"
+  }
+];
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-export default class Shelf extends Component {
-    constructor(props) {
-        super(props)
-    }
-    
-    render() {
+export default function Shelf(props) {
         return (
             <div className="bookshelf">
-            <h2 className="bookshelf-title">{this.props.title}</h2>
+            <h2 className="bookshelf-title">{props.title}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                {this.props.list.map(book => (
-                    <Book shelf={book.shelf} title={book.title} author={book.authors ? book.authors[0] : "N/A"} imgSrc={this.props.title === "Search Results" ? book.img : book.imageLinks.thumbnail} key={book.id} id={book.id} changeList={this.props.changeList}/>
+                {props.list.map(book => (
+                    <Book shelf={book.shelf} title={book.title} author={book.authors ? book.authors[0] : "N/A"} imgSrc={props.title === "Search Results" ? book.img : book.imageLinks.thumbnail} key={book.id} id={book.id} changeList={props.changeList}/>
                 ))}
                 </ol>
             </div>
             </div>
         )
-    }
 }
